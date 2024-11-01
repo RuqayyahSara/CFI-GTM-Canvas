c4342eff0b350a53ea313eec7cdae728f98e65e8
sai shravan <saishravan384@gmail.com>
2024-11-01 13:31:06 +0530

    feat: Add feedback section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -364,6 +364,42 @@ export default function Home() {
           </div>
         </section>
 
+        <section className='clients'>
+          <p className='heading'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              height='24px'
+              viewBox='0 -960 960 960'
+              width='24px'
+              fill='#000000'
+            >
+              <path d='m178.18-480 284.95 284.96q18.39 18.39 18.18 44.04-.22 25.65-18.61 44.04-18.39 18.4-44.33 18.4-25.93 0-44.33-18.4L75.09-404.91q-15.96-15.96-23.65-35.48-7.7-19.52-7.7-39.61t7.7-39.61q7.69-19.52 23.65-35.48l298.95-298.95q18.4-18.4 44.55-18.18 26.15.22 44.54 18.61t18.39 44.33q0 25.93-18.39 44.32L178.18-480Z' />
+            </svg>
+            <span className='heading'>What Our Clients Say About Us</span>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              height='24px'
+              viewBox='0 -960 960 960'
+              width='24px'
+              fill='#000000'
+            >
+              <path d='M560.91-480 275.96-764.96q-18.4-18.39-18.18-44.26.22-25.87 18.61-44.26 18.39-18.39 44.54-18.39 26.16 0 44.55 18.39L664-555.09q15.96 15.96 23.65 35.48 7.7 19.52 7.7 39.61t-7.7 39.61q-7.69 19.52-23.65 35.48L365.04-105.96q-18.39 18.4-44.04 17.9t-44.04-18.9q-18.4-18.39-18.4-44.54t18.4-44.54L560.91-480Z' />
+            </svg>
+          </p>
+
+          <p className='dots'>
+            <span className='active'></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </p>
+
+          <div className='clientsContainer'>
+            <img src='/client.png' alt='client' />
+          </div>
+        </section>
+
         <section className='handbook'>
           <div className='handbookContainer'>
             <img src='/monitor.png' alt='monitor' />
