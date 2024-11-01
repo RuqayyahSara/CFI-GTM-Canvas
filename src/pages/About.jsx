a19648040884c0a7900ef3ada5d78073f23c2d80
sai shravan <saishravan384@gmail.com>
2024-11-01 10:42:47 +0530

    feat: Add excellence section in the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,37 @@
-import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import '../styles/About.css'
 
 export default function About() {
   return (
     <>
       <Navbar />
       <div className='about'>
-        <p>About</p>
+        <section className='excellence'>
+          <div className='left'>
+            <p className='heading'>
+              GTM<i>Canvas</i>: <br /> Founded On Excellence.
+            </p>
+            <p>
+              Meraj Faheem, the visionary behind the GTM certification program,
+              brings over a decade of expertise in helping businesses thrive in
+              competitive markets.
+            </p>
+            <p>
+              A seasoned serial entrepreneur, Meraj has built multiple
+              first-to-market, 0-7 figure businesses.He has worked with
+              government agencies, corporates, Big 4, and has helped over 300+
+              startups build their products and take them to market.
+            </p>
+          </div>
+          <div className='right'>
+            <p className='first'>1 IN 6</p>
+            <p className='second'>
+              1 in 6 business schools/ organisations use GTM Canvas as their
+              primary GTM strategy tool
+            </p>
+          </div>
+        </section>
       </div>
       <Footer />
     </>
